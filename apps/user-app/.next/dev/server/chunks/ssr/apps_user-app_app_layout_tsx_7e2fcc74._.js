420f286e082cfd97eaef65f01b9e16ff382c6993
AjCodes <ajabood7788@gmail.com>
2025-12-19 10:13:47 +0100

    Initial commit

---
+++ apps/user-app/.next/dev/server/chunks/ssr/apps_user-app_app_layout_tsx_7e2fcc74._.js
@@ -0,0 +1,7 @@
+module.exports = [
+"[project]/apps/user-app/app/layout.tsx [app-rsc] (ecmascript)", ((__turbopack_context__, module, exports) => {
+
+}),
+];
+
+//# sourceMappingURL=apps_user-app_app_layout_tsx_7e2fcc74._.js.map
